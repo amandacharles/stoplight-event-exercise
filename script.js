176062eb2f539b3c43ffb35874cbfad40f3537c2
Amanda Charles <ancharles11@gmail.com>
2016-11-10 15:51:59 -0800

    added light documentss

--- script.js
+++ script.js
@@ -8,3 +8,30 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+
+// RED LIGHT ******************
+
+const redLightButton = document.querySelector('#stopButton');
+
+redLightButton.addEventListener('click', () => {
+const redLightDiv = document.getElementById("stopLight");
+redLightDiv.className = "redBulb";
+})
+
+// YELLOW LIGHT ********************
+const yellowLightButton = document.querySelector('#slowButton');
+
+yellowLightButton.addEventListener('click', () => {
+const yellowLightDiv = document.getElementById('slowLight');
+yellowLightDiv.className = "yellowBulb";
+})
+
+// GREEN LIGHT ***********************
+
+const greenLightButton = document.querySelector('#goButton');
+
+greenLightButton.addEventListener('click', () => {
+  const greenLightDiv = document.getElementById('goLight');
+  greenLightDiv.className = "greenBulb";
+})
